52fb56acebbd96edc88abe5e8a03497b42ecff08
Ryan Gallagher <rgallagher35@qub.ac.uk>
2018-10-06 17:38:44 +0100

    mend

---
+++ Assets/Scripts/SpecialAbilities/AbilityBase.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbilityBase : MonoBehaviour {
+
+    public abstract void useAbility(Collider2D playerCollider, Vector3 playerPosition, Vector3 crosshairPosition);
+}
