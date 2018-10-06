55a2dd12a0dcad25e2c268002f33fcee1cf3ca39
Robert Finn <rfinn05@qub.ac.uk>
2018-10-06 19:24:05 +0100

    Adding movement classes:     - PlayerMovementController: controls player movement     - SquadMovementController: controls squad movement     - SquadOffsets: updates the location of where squad members should     be     - MovementManager: controls references of PlayerMovement and     SquadMovement Controllers to Players / Squad Members     - Member: A squad member / player

---
+++ Assets/Scripts/Movement/MovementManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour {
+
+    // Access to player and squad controllers
+    public PlayerMovementController playerController;
+    public SquadMovementController squadController;
+    public SquadOffsets offsets;
+
+    // Key to change player
+    public KeyCode changePlayer;
+
+    private int playerIndex;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerIndex = 0;
+        offsets.SetArrangement(SquadOffsets.Arrangement.DIAMOND);
+	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(changePlayer))
+        {
+            Member playerReference = playerController.player;
+            playerController.player = squadController.members[playerIndex];
+            squadController.members[playerIndex] = playerReference;
+            offsets.player = playerController.player;
+            playerIndex = (playerIndex += 1) % 3;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
