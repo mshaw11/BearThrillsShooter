1ff76cb8eb29742494f1841a3407854f086a3924
Matthew Logan Shaw <mshaw11@qub.ac.uk>
2018-10-10 22:52:06 +0100

    Adding refined and refactored swarm AI using pathfinding and flocking behaviour in stand alone scene.

---
+++ Assets/Scripts/Swarms/Level.cs
@@ -0,0 +1,41 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Level : MonoBehaviour {
+//    public Transform memberPrefab;
+//    public Transform enemyPrefab;
+//    public int numberOfMembers;
+//    public int numberOfEnemies;
+//    public List<Member> members;
+//    public List<Enemy> enemies;
+//    public float bounds;
+//    public float spawnRadius;
+
+//	// Use this for initialization
+//	void Start()
+//    {
+//        members = new List<Member>();
+//        enemies = new List<Enemy>();
+
+//        //spawn members
+//        Spawn(memberPrefab, numberOfMembers);
+//        Spawn(enemyPrefab, numberOfEnemies);
+
+//        members.AddRange(FindObjectsOfType<Member>());
+//        enemies.AddRange(FindObjectsOfType<Enemy>());
+//    }
+	
+//    void Spawn(Transform prefab, int count)
+//    {
+//        for (int i = 0; i < count; i++)
+//        {
+//            //Instantiate(prefab, new Vector3(Random.Range(-spawnRadius, spawnRadius), Random.Range(-spawnRadius, spawnRadius), 0), Quaternion.identity);
+//            Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+//        }
+//    }
+
+
+
+
+//}
---
+++ Assets/Scripts/Swarms/SwarmMemberConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmMemberConfig : MonoBehaviour {
+
+    public float maxFOV;
+    public float maxAcceleration;
+    public float maxVelocity;
+    public float maxForce;
+
+    //Pathfind
+    public float pathfindPriority;
+
+    //Cohesion
+    public float cohesionRadius;
+    public float cohesionPriority;
+
+    //Alignment
+    public float alignmentRadius;
+    public float alignmentPriority;
+
+    //Separation
+    public float separationRadius;
+    public float separationPriority;
+
+    //Avoidance
+    public float avoidanceRadius;
+    public float avoidancePriority;
+
+}
