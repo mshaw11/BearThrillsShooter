1c6a858b883876767072c64dccab95a83c35307b
Stephan Williams <stephan1994w@gmail.com>
2018-11-04 10:10:29 +0000

    AbilityUI commit

---
+++ Assets/Scripts/AbilityUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityUI : MonoBehaviour {
+
+    [SerializeField]
+    private List<GameObject> abilityUIObjs;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void enableColor(GameObject abilityImage)
+    {
+        abilityImage.GetComponent<Image>().color = new Color(1,1,1,1);
+    }
+
+    void disableColor(GameObject abilityImage)
+    {
+        abilityImage.GetComponent<Image>().color = new Color(0, 0, 0, 0.5f);
+    }
+
+    public void SetAbility(int num)
+    {
+        Debug.Log("Ability: " + num);
+        foreach (GameObject g in abilityUIObjs)
+        {
+            if (abilityUIObjs.IndexOf(g) == num)
+            {
+                enableColor(g);
+            }
+            else
+            {
+                disableColor(g);
+            }
+        }
+    }
+}
